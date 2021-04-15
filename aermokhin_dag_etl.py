29cbd55335d99e6b4d9941c6b3971c85c08681a7
OrBeTGroup <48784181+OrBeTGroup@users.noreply.github.com>
2021-04-15 17:55:15 +0300

    Update aermokhin_dag_etl.py

--- aermokhin_dag_etl.py
+++ aermokhin_dag_etl.py
@@ -23,34 +23,34 @@ ods = []
 for task in tasks:
     if task == 'traffic':
         query = """
-                insert overwrite table aermokhin.ods_traffic partition (year='{{executiondate_date.year}}')
+                insert overwrite table aermokhin.ods_traffic partition (year='{{execution_date.year}}')
                 select user_id, cast(from_unixtime(`timestamp` div 1000) as TIMESTAMP), device_id, device_ip_addr, bytes_sent, bytes_received
-                       from aermokhin.stg_traffic where year(from_unixtime(`timestamp` div 1000))= {{executiondate_date.year}};
+                       from aermokhin.stg_traffic where year(from_unixtime(`timestamp` div 1000))= {{execution_date.year}};
                 """
     elif task == 'billing':
         query = """
-                insert overwrite table aermokhin.ods_billing partition (year='{{executiondate_date.year}}')
+                insert overwrite table aermokhin.ods_billing partition (year='{{execution_date.year}}')
                 select user_id, billing_period, service, tariff, cast(sum as DECIMAL(10,2)), cast(created_at as TIMESTAMP)
-                       from aermokhin.stg_billing where year(created_at) = {{executiondate_date.year}};
+                       from aermokhin.stg_billing where year(created_at) = {{execution_date.year}};
                 """
     elif task == 'issue':
         query = """
-                insert overwrite table aermokhin.ods_issue partition (year='{{executiondate_date.year}}')
+                insert overwrite table aermokhin.ods_issue partition (year='{{execution_date.year}}')
                 select cast(user_id as INT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service
-                       from aermokhin.stg_issue where year(start_time) = {{executiondate_date.year}};
+                       from aermokhin.stg_issue where year(start_time) = {{execution_date.year}};
 
                 """
     elif task == 'payment':
         query = """
-                insert overwrite table aermokhin.ods_payment partition (year='{{executiondate_date.year}}')
+                insert overwrite table aermokhin.ods_payment partition (year='{{execution_date.year}}')
                 select user_id, pay_doc_type, pay_doc_num, account, phone, billing_period, cast(pay_date as TIMESTAMP), cast(sum as DECIMAL(10,2))
-                from aermokhin.stg_payment where year(pay_date) = {{executiondate_date.year}};
+                from aermokhin.stg_payment where year(pay_date) = {{execution_date.year}};
                 """
     ods.append(DataProcHiveOperator(
         task_id='ods_'+task,
         dag=dag,
         query=query,
-        job_name=username + '_{{executiondate_date.year}}_ods_' + task + '_{{params.job_suffix}}',
+        job_name=username + '_{{execution_date.year}}_ods_' + task + '_{{params.job_suffix}}',
         params={"job_suffix": randint(0, 100000)},
         cluster_name='cluster-dataproc',
         region='europe-west3',
@@ -60,11 +60,11 @@ dm = DataProcHiveOperator(
     task_id='dm_traffic',
     dag=dag,
     query = """
-            insert overwrite table aermokhin.dm_traffic partition (year = '{{executiondate_date.year}}')
+            insert overwrite table aermokhin.dm_traffic partition (year = '{{execution_date.year}}')
             select user_id, max(bytes_received), min(bytes_received), round(avg(bytes_received)) as avg_traf
-                   from aermokhin.ods_traffic where year = {{executiondate_date.year}} group by user_id order by avg_traf;
+                   from aermokhin.ods_traffic where year = {{execution_date.year}} group by user_id order by avg_traf;
             """,
-    job_name=username + '_{{executiondate_date.year}}_dm_traffic_{{params.job_suffix}}',
+    job_name=username + '_{{execution_date.year}}_dm_traffic_{{params.job_suffix}}',
     params={"job_suffix": randint(0, 100000)},
     cluster_name='cluster-dataproc',
     region = 'europe-west3',
