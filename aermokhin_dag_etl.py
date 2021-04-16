73f5cb948c4c3e7699e861fc7d52409eacafb573
OrBeTGroup <48784181+OrBeTGroup@users.noreply.github.com>
2021-04-16 10:46:27 +0300

    Update aermokhin_dag_etl.py

--- aermokhin_dag_etl.py
+++ aermokhin_dag_etl.py
@@ -55,18 +55,18 @@ for task in tasks:
         cluster_name='cluster-dataproc',
         region='europe-west3',
     ))
-
-dm = DataProcHiveOperator(
-    task_id='dm_traffic',
-    dag=dag,
-    query = """
-            insert overwrite table aermokhin.dm_traffic partition (year = '{{execution_date.year}}')
-            select user_id, max(bytes_received), min(bytes_received), round(avg(bytes_received)) as avg_traf
-                   from aermokhin.ods_traffic where year = {{execution_date.year}} group by user_id order by avg_traf;
-            """,
-    job_name=username + '_{{execution_date.year}}_dm_traffic_{{params.job_suffix}}',
-    params={"job_suffix": randint(0, 100000)},
-    cluster_name='cluster-dataproc',
-    region = 'europe-west3',
-)
-ods >> dm
+    if task == 'traffic':
+        dm = DataProcHiveOperator(
+            task_id='dm_traffic',
+            dag=dag,
+            query = """
+                    insert overwrite table aermokhin.dm_traffic partition (year = '{{execution_date.year}}')
+                    select user_id, max(bytes_received), min(bytes_received), round(avg(bytes_received)) as avg_traf
+                           from aermokhin.ods_traffic where year = {{execution_date.year}} group by user_id order by avg_traf;
+                    """,
+        job_name=username + '_{{execution_date.year}}_dm_traffic_{{params.job_suffix}}',
+        params={"job_suffix": randint(0, 100000)},
+        cluster_name='cluster-dataproc',
+        region = 'europe-west3',
+    )
+    ods >> dm
